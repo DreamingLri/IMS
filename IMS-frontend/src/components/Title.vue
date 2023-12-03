87f3dd6b630392e3a7e7631c5df2719b787f935b
DreamingLri <3507415455@qq.com>
2023-12-03 12:47:29 +0800

    basic get, insert, update, delete method of user table

---
+++ IMS-frontend/src/components/Title.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>DOUBLE-DUCK UNIVERSITY Academic Management System</h1>
+  <el-divider/>
+  <h3>双鸭山大学教务管理系统</h3>
+</template>
+
+<style scoped>
+
+</style>
